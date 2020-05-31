327c2957a3252d03c441ab462f10905a55adfa52
xavi <xabi1309@gmail.com>
2020-05-31 16:06:01 +0200

    Search an artists top track

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com'
+
 class App extends Component{
-    state = { artistQuery: '' };
+    state = { artistQuery: '', artist: null, tracks: [] };
 
     toggleDisplayBio = () => {
         this.setState({displayBio: !this.state.displayBio});
     }
 
     updateArtistQuery = event => {
-        console.log('event.target.value', event.target.value);
-        this.setState({artistQuery: event.target.value})
+        this.setState({artistQuery: event.target.value});
     }
 
     searchArtist = () => {
-        console.log('this.state', this.state)
+        fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+            .then(response => response.json())
+            .then(json => {
+
+                if(json.artists.total > 0){
+                    const artist = json.artists.items[0];
+
+                    this.setState({artist});
+
+                    fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+                        .then(response => response.json())
+                        .then(json => this.setState({tracks: json.tracks}))
+                        .catch(error => alert(error.message));
+                    }
+                })
+            .catch(error => alert(error.message));
     }
 
     handleKeyPress = event => {
@@ -23,6 +39,7 @@ class App extends Component{
     }
 
     render() {
+        console.log(this.state);
         return(
             <div>
                 <h2>Music Master</h2>
